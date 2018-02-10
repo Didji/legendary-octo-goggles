4c3c58ae4de6dd4c54e6a8f5ef646383a95b3826
Jean-François Briatte <jfbriatte@gmail.com>
2018-02-10 19:39:23 +0100

    Ajout de fichiers manquants

---
+++ Assets/scripts/obstacle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class obstacle : MonoBehaviour {
+
+    // Use this for initialization
+
+    joueur _joueur;
+   
+    void Start ()
+    {
+        _joueur = GameObject.Find("Sphere").GetComponent<joueur>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	   
+	}
+
+    
+}
+
