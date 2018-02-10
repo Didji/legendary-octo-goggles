96e232bbeb6d64952133508be9f6b0d8b7efabf6
jean francois briatte <jeffbri007@hotmail.fr>
2018-02-10 16:43:41 +0100

    Commit initial

---
+++ Assets/scripts/cube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour {
+
+
+    joueur _joueur;
+
+	// Use this for initialization
+	void Start () {
+
+        _joueur = GameObject.Find("Sphere").GetComponent<joueur>();
+
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        print(_joueur.life);
+
+
+    }
+}
---
+++ Assets/scripts/joueur.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class joueur : MonoBehaviour {
+
+
+    public int life = 10;
+    CharacterController char_cont;
+    Vector3 init_pos;
+
+	// Use this for initialization
+	void Start ()
+    {
+        char_cont = GetComponent<CharacterController>();
+        life = 5;
+        init_pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            print(Input.GetAxis("Horizontal"));
+
+            //transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * 2f * Time.deltaTime, transform.position.y, transform.position.z);
+            char_cont.Move(transform.right*Time.deltaTime*2f);//(new Vector3(transform.position.x + Input.GetAxis("Horizontal") * 0.01f * Time.deltaTime, transform.position.y, transform.position.z));
+
+
+        
+        }
+
+        transform.position = new Vector3(transform.position.x, init_pos[1], init_pos[2]);
+	}
+}
